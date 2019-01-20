af783ea1333df0eb9b2111150723bb5258b531a7
Vladislav Levchenko <levchenkoo.vladislav@gmail.com>
2019-01-20 20:43:25 +0600

    manifest and service workers

---
+++ sw.js
@@ -0,0 +1,32 @@
+var CACHE = 'pomodoro-v1';
+
+
+ 
+self.addEventListener('install', function(evt) {
+  console.log('The service worker is being installed.');
+  evt.waitUntil(precache());
+});
+
+ 
+self.addEventListener('fetch', function(evt) {
+  console.log('The service worker is serving the asset.');
+  evt.respondWith(fromCache(evt.request));
+});
+
+function precache() {
+    return caches.open(CACHE).then(function (cache) {
+      return cache.addAll([
+        'vue-pomodoro/script.js',
+        'vue-pomodoro/style.css',
+        'vue-pomodoro/index.html'
+      ]);
+    });
+}
+
+function fromCache(request) {
+    return caches.open(CACHE).then(function (cache) {
+      return cache.match(request).then(function (matching) {
+        return matching || Promise.reject('no-match');
+      });
+    });
+}
