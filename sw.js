a4b4bda56853cfe4d34910e7af52571fdf951cde
Vladislav Levchenko <levchenkoo.vladislav@gmail.com>
2019-01-20 21:13:29 +0600

    manifest and service workers

--- sw.js
+++ sw.js
@@ -1,53 +1,51 @@
-var CACHE = 'pomodoro-v1';
+var CACHE_NAME = 'pomodoro-cache-v1';
+var urlsToCache = [
+  '/',
+  'style.css',
+  'script.js',
+];
 
-
- 
-self.addEventListener('install', function(evt) {
-  console.log('The service worker is being installed.');
-  evt.waitUntil(precache());
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
- 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request)
       .then(function(response) {
+        // Cache hit - return response
         if (response) {
-          return response;     // if valid response is found in cache return it
-        } else {
-          return fetch(event.request)     //fetch from internet
-            .then(function(res) {
-              return caches.open(CACHE_DYNAMIC_NAME)
-                .then(function(cache) {
-                  cache.put(event.request.url, res.clone());    //save the response for future
-                  return res;   // return the fetched data
-                })
-            })
-            .catch(function(err) {       // fallback mechanism
-              return caches.open(CACHE_CONTAINING_ERROR_MESSAGES)
-                .then(function(cache) {
-                  return cache.match('/offline.html');
-                });
-            });
+          return response;
         }
+
+        return fetch(event.request).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
       })
-  );
-});      
-
-function precache() {
-    return caches.open(CACHE).then(function (cache) {
-      return cache.addAll([
-        'script.js',
-        'style.css',
-        'index.html'
-      ]);
-    });
-}
-
-function fromCache(request) {
-    return caches.open(CACHE).then(function (cache) {
-      return cache.match(request).then(function (matching) {
-        return matching || Promise.reject('no-match');
-      });
-    });
-}
+    );
+});
